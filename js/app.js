9d6359508a175da180a14091561895e15d3e0837
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-10-07 09:57:18 -0300

    Inicio da lógica do slider

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
 import { data } from './_data.js';
 
-console.log(data);
+const $storiesCardList = document.querySelector('.stories-card-list');
+const $storiesPrev = document.querySelectorAll('.stories-prev');
+const $storiesNext = document.querySelectorAll('.stories-next');
+
+const windowWidth = $storiesCardList.clientWidth;
+$storiesCardList.setAttribute('data-position', '0');
+
+$storiesPrev.forEach((item, index) => {
+	item.addEventListener('click', () => {
+		const currentPosition = $storiesCardList.getAttribute('data-position');
+		const newPosition = (Math.abs(currentPosition) - windowWidth);
+
+		$storiesCardList.style.transform = `translateX(-${newPosition}px)`;
+		$storiesCardList.setAttribute('data-position', `${newPosition}`);
+	});
+});
+
+$storiesNext.forEach((item, index) => {
+	item.addEventListener('click', () => {
+		const currentPosition = $storiesCardList.getAttribute('data-position');
+		const newPosition = (Math.abs(currentPosition) + windowWidth);
+
+		$storiesCardList.style.transform = `translateX(-${newPosition}px)`;
+		$storiesCardList.setAttribute('data-position', `${newPosition}`);
+	});
+});
